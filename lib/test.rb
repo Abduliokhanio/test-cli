078280fada9aec74c2fb237a05c6d22dab185dc9
Abdul Khan <akhan17@outlook.com>
2020-04-27 20:50:53 +0000

    Returns list of top 10

--- lib/test.rb
+++ lib/test.rb
@@ -3,21 +3,29 @@ require 'open-uri'
 require 'httparty'
 require 'pry'
 
-  html = open("https://store.steampowered.com/search/?specials=1")
-  doc = Nokogiri::HTML(open(html))
-
-
   #doc.css("div.responsive_search_name_combined")[0].text.split                       # array
   #doc.css("span.title")[0].text                                                      # title
   #doc.css("div.responsive_search_name_combined")[0].text.split[-1].split("$")[1]     # origional price
   #doc.css("div.responsive_search_name_combined")[0].text.split[-1].split("$")[2]     # discounted price
   #doc.css("div.responsive_search_name_combined")[0].text.split[-2]                   # Percent off
 
-  #doc.css("span.title")[0].text #title
+def top_10
+  html = open("https://store.steampowered.com/search/?specials=1")
+  doc = Nokogiri::HTML(open(html))
 
+  puts "Count  |  Title  |  Original Price  |  Percent off  |  Discounted Price"
+  puts "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 
+  epoc = 0
+  while epoc < 10
+    title = doc.css("span.title")[epoc].text
+    origional_price =  doc.css("div.responsive_search_name_combined")[epoc].text.split[-1].split("$")[1]
+    discounted_price = doc.css("div.responsive_search_name_combined")[epoc].text.split[-1].split("$")[2]
+    percent_off = doc.css("div.responsive_search_name_combined")[epoc].text.split[-2]
 
-  puts doc.css("span.title")[0].text
-  puts doc.css("div.responsive_search_name_combined")[0].text.split[-1].split("$")[1]
-  puts doc.css("div.responsive_search_name_combined")[0].text.split[-1].split("$")[2]
-  puts doc.css("div.responsive_search_name_combined")[0].text.split[-2]
+    puts "#{epoc + 1}  |  #{title}  | Original Price: $#{origional_price}  | Percent off: #{percent_off}  | Discounted Price: $#{discounted_price}"
+    epoc += 1
+  end
+end
+
+top_10
