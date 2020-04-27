7c0fc776f53e9b5f05e2d578534688bf5f2da880
Abdul Khan <akhan17@outlook.com>
2020-04-27 04:40:26 +0000

    deleted extra files

--- lib/test/cli.rb
+++
@@ -1,8 +0,0 @@
-require "test/cli/version"
-
-module Test
-  module Cli
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
--- lib/test/cli/version.rb
+++
@@ -1,5 +0,0 @@
-module Test
-  module Cli
-    VERSION = "0.1.0"
-  end
-end
