6cf81556070b222fe2259e9e05f6ca9dd083a36a
Abdul Khan <akhan17@outlook.com>
2020-04-30 23:28:47 +0000

    added hash to scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,16 +7,23 @@ class Scraper
 
   attr_accessor :doc, :title, :original_price, :percent_off, :discounted_price
 
-  @@all = []
+  @@all = {}
   def initialize(url)
     html = open(url)
     @doc = Nokogiri::HTML(open(html))
   end
 
   def collecter
+
     epoc = 0
     while epoc < 10
-      @@all << @doc.css("span.title")[epoc].text
+      title = @doc.css("span.title")[epoc].text
+      origional_price =  @doc.css("div.responsive_search_name_combined")[epoc].text.split[-1].split("$")[1]
+      discounted_price = @doc.css("div.responsive_search_name_combined")[epoc].text.split[-1].split("$")[2]
+      percent_off = @doc.css("div.responsive_search_name_combined")[epoc].text.split[-2]
+
+
+      @@all[epoc +1] = [title,origional_price,discounted_price,percent_off]
       epoc += 1
     end
   end
