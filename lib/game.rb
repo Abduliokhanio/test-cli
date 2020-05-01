99b6cd90ea5ea3c13de73d47398cb8b301aa3b2b
Abdul Khan <akhan17@outlook.com>
2020-05-01 23:34:00 +0000

    Connected new data from Scraper to Class

--- lib/game.rb
+++ lib/game.rb
@@ -1,15 +1,21 @@
 class Game
 
-  attr_accessor :title, :original_price, :percent_off, :discounted_price
+  attr_accessor :title, :original_price, :percent_off, :discounted_price, :snip_bit, :game_review, :developer, :des, :min_req, :rec_req
 
   @@all = []
   @@count = 0
-  def initialize(title, original_price, percent_off, discounted_price)
+  def initialize(title, original_price, percent_off, discounted_price, snip_bit, game_review, developer, des, min_req, rec_req)
 
     @title = title
     @original_price = original_price
     @percent_off = percent_off
     @discounted_price = discounted_price
+    @snip_bit =  snip_bit
+    @game_review = game_review
+    @developer = developer
+    @des =  des
+    @min_req =  min_req
+    @rec_req = rec_req
 
     @@all << self
   end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,8 +23,16 @@ class Scraper
       origional_price =  @doc.css("div.responsive_search_name_combined")[epoc].text.split[-1].split("$")[1]
       discounted_price = @doc.css("div.responsive_search_name_combined")[epoc].text.split[-1].split("$")[2]
       percent_off = @doc.css("div.responsive_search_name_combined")[epoc].text.split[-2]
+      deep_dive_scraper(epoc)
+      snip_bit = @doc2_deep_info.css("div.game_description_snippet").text.strip #snip_bit
+      game_review = @doc2_deep_info.css("span.game_review_summary")[0].text rescue "Review Not Listed" #game review
+      #binding.pry
+      developer = @doc2_deep_info.css("div.dev_row")[0].text.strip.split(',')[0].split[1] rescue nil#dev team
+      des = @doc2_deep_info.css("div.game_area_description").text #game discription
+      min_req = @doc2_deep_info.css("div.game_area_sys_req_leftCol").text.strip #minimum Req
+      rec_req = @doc2_deep_info.css("div.game_area_sys_req_rightCol").text.strip #reccomended Req
 
-      @@all[epoc ] = [title,origional_price,discounted_price,percent_off]
+      @@all[epoc] = [title,origional_price,discounted_price,percent_off,snip_bit,game_review,developer,des,min_req,rec_req]
       epoc += 1
     end
     creator
@@ -37,7 +45,13 @@ class Scraper
        origional_price1 = @@all[epoc][1]
        discounted_price1 = @@all[epoc][2]
        percent_off1 = @@all[epoc][3]
-       Game.new(title1, origional_price1, percent_off1, discounted_price1)
+       snip_bit1 = @@all[epoc][4]
+       game_review1 = @@all[epoc][5]
+       developer1= @@all[epoc][6]
+       des1= @@all[epoc][7]
+       min_req1= @@all[epoc][8]
+       rec_req1= @@all[epoc][9]
+       Game.new(title1, origional_price1, percent_off1, discounted_price1, snip_bit1, game_review1, developer1, des1, min_req1, rec_req1)
      epoc += 1
    end
   end
@@ -46,23 +60,24 @@ class Scraper
     puts @@all
   end
 
-  def description
-    new_url = @doc.css("a.search_result_row")[0]["href"] # getting the link
+  def deep_dive_scraper(epoch)
+    new_url = @doc.css("a.search_result_row")[epoch]["href"] # getting the link
     html = open(new_url)
     @doc2_deep_info = Nokogiri::HTML(open(html))
-    snip_bit = @doc2_deep_info.css("div.game_description_snippet").text.strip #snip_bit
-    game_review = @doc2_deep_info.css("span.game_review_summary")[0].text #game review
-    developer = @doc2_deep_info.css("div.dev_row")[0].text.strip.split(',')[0].split[1] #dev team
-    des = @doc2_deep_info.css("div.game_area_description").text #game discription
-    min_req = @doc2_deep_info.css("div.game_area_sys_req_leftCol").text.strip #minimum Req
-    rec_req = @doc2_deep_info.css("div.game_area_sys_req_rightCol").text.strip #reccomended Req
+
+    # snip_bit = @doc2_deep_info.css("div.game_description_snippet").text.strip #snip_bit
+    # game_review = @doc2_deep_info.css("span.game_review_summary")[0].text #game review
+    # developer = @doc2_deep_info.css("div.dev_row")[0].text.strip.split(',')[0].split[1] #dev team
+    # des = @doc2_deep_info.css("div.game_area_description").text #game discription
+    # min_req = @doc2_deep_info.css("div.game_area_sys_req_leftCol").text.strip #minimum Req
+    # rec_req = @doc2_deep_info.css("div.game_area_sys_req_rightCol").text.strip #reccomended Req
 
     #puts snip_bit
     #puts game_review
     #puts developer
     #puts des
-    puts min_req
-    puts rec_req
+    #min_req
+    #rec_req
   end
 
 end
@@ -77,8 +92,7 @@ action_url = "https://store.steampowered.com/search/?specials=1&tags=19"
 genral = Scraper.new(general_url)
 genral.collecter
 
-genral.description
-
-# Game.all.each do |val|
-#   puts val.title
-# end
+Game.all.each do |val|
+  puts val.title
+  puts val.game_review
+end
