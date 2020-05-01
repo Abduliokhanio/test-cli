d967691578e4e13dde676e1bf3632f84acede598
Abdul Khan <akhan17@outlook.com>
2020-05-01 05:26:22 +0000

    added logic to display

--- lib/display.rb
+++ lib/display.rb
@@ -19,7 +19,46 @@ class Display
     puts "6 : Action"
   end
 
+  def logic
+    boogie_woogie = true
+
+    while boogie_woogie == true
+      input = gets.chomp
+
+      if input == '1'
+
+      elsif input == '2'
+
+      elsif input == '3'
+
+      elsif input == '4'
+
+      elsif input == '5'
+
+      elsif input == '6'
+
+      elsif input == "q"
+        puts "Bye for now!"
+        boogie_woogie = false
+      else
+        puts "That's not an option."
+        selection
+      end
+
+    end
+
+
+
+  end
+
+
   def output
   end
 
 end
+
+
+uno = Display.new
+uno.welcome
+uno.selection
+uno.logic
